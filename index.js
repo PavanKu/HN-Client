4187e814e4861f3f3f42bba23a37d4eb8fd98610
Pavan Kumar <pavankumar7860@gmail.com>
2015-07-05 18:06:23 +0530

    Second commit.
    1) Updated semantic-ui version to 2.0
    2) Updated UI
    3) Using duckduckgo's service to fetch favicon

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var client = require("./client");
 
 var app = express();
 var HN_baseurl = "https://hacker-news.firebaseio.com/v0/";
+var topStoryUrl = HN_baseurl + "topstories.json";
+var itemUrl = HN_baseurl + "item/";
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -17,21 +19,45 @@ app.get("/", function (req, res) {
 	});
 });
 
-app.get("/getStories", function(req, res){
-	var topStoryUrl = HN_baseurl + "topstories.json";
+app.get("/getTopStories", function(req, res){
+
 	
 	client.get(topStoryUrl, function(err, data){
 		if(err) return;
 		
 		app.locals.topStoryIds = data;
+		app.locals.topStoryOffset = 0;
 		
-		var getIteamUrl = "http://localhost:3000/getItems?offset=0&count=15";
-		client.get(getIteamUrl,function(err, data){
-			if(err) return;
-			
-			res.json(data);
-			res.end();
-		});
+		res.json(app.locals.topStoryIds.slice(app.locals.topStoryOffset,app.locals.topStoryOffset + 19));
+		app.locals.topStoryOffset += 20;
+		res.end();
+
+//
+//		var getIteamUrl = "http://localhost:3000/getItems?offset=0&count=15";
+//		client.get(getIteamUrl,function(err, data){
+//			if(err) return;
+//
+//			res.json(data);
+//			res.end();
+//		});
+	});
+});
+
+app.get("/getNextTopStories", function(req, res){
+
+	res.json(app.locals.topStoryIds.slice(app.locals.topStoryOffset,app.locals.topStoryOffset +19));
+	app.locals.topStoryOffset += 20;
+	res.end();
+});
+
+app.get("/getItem", function(req, res){
+	var itemId = req.query.id;
+
+	client.get([itemUrl, itemId, ".json"].join(""), function(err, data){
+		if(err) return;
+
+		res.json(data);
+		res.end();
 	});
 });
 
--- public/app/app.js
+++ public/app/app.js
@@ -1,42 +1,37 @@
 (function (w, d, undefined) {
 	$(document).ready(function () {
-		var nextUrl = '';
 
 		//Fetch HN Posts
-		$.getJSON("/getStories", function (stories) {
-			console.log(stories);
-			var markUp = "";
-			stories.forEach(function (story) {
-				if (!story.next) {
-					markUp += '<div class="item"><img class="ui avatar image" src="' + getFavicon(story.url) + '"><div class="content"><a href="' + story.url + '" class="header">' + story.title + '</a><div class="description"></div></div></div>';
-
-					$("#topList").html(markUp);
-				} else {
-					nextUrl = story.next;
+		$.getJSON("/getTopStories")
+			.done(function (stories) {
+				console.log(stories);
+				for (var i = 0; i < stories.length; i++) {
+					var storyId = stories[i];
+					$.getJSON("/getItem?id=" + storyId)
+						.done(function (story) {
+							console.log(story);
+							$("#topStoriesList").append(getCardMarkup(story));
+						});
 				}
 			});
-		});
 
-		$("#nextBtn").click(function () {
-			$.getJSON(nextUrl, function (stories) {
-				var markUp = "";
-				stories.forEach(function (story) {
-					if (!story.next) {
-						markUp += '<div class="item"><img class="ui avatar image" src="' + getFavicon(story.url) + '"><div class="content"><a href="' + story.url + '" class="header">' + story.title + '</a><div class="description"></div></div></div>';
+		function getCardMarkup(story) {
+//			var cardMarkUp = '<div class="ui float card"><div class="content"><div class="right floated meta">14h</div><img class="ui avatar mini image" src="' + getFavicon(story.url) + '"> ' + story.title + '</div><div class="image"><a href="' + story.url + '" class="ui medium image"><img class="ui image" src="http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2015/07/nope1.jpg"></a></div><div class="content"><span class="right floated"><i class="heart outline like icon"></i>17 likes</span><i class="comment icon"></i> 3 comments</div><div class="extra content"><div class="ui large transparent left icon input"><i class="heart outline icon"></i><input type="text" placeholder="Add Comment..."></div></div></div>';
+			story.kids = story.kids || [];
+			var cardMarkup = '<div class="ui card"><div class="image"><img src="http://www.semantic-ui.com/images/avatar2/large/kristy.png">\
+							  </div><div class="content"><a class="header" href="'+story.url+'">'+story.title+'</a>\
+								<div class="meta"><span class="date">Joined in 2013</span></div><div class="description">Kristy is an art director living in New York.</div></div><div class="extra content"><img class="ui image mini avatar" src="'+getFavicon(story.url)+'"><a class="right floated"><i class="comment icon"></i>'+story.kids.length+' comments</a></div></div>';
 
-						$("#topList").html(markUp);
-					} else {
-						nextUrl = story.next;
-					}
-				});
-			});
-		});
+			return cardMarkup;
+		}
 
 		function getFavicon(url) {
-			var service = "http://favicon.yandex.net/favicon/";
+//						var service = "https://logo.clearbit.com/";
+//						var service = "http://favicon.yandex.net/favicon/";
+			var service = "//icons.duckduckgo.com/ip2/"
 			var domain = url.split("/")[2] || "";
 
-			return service + domain;
+			return service + domain + ".ico";
 		}
 	});
 })(window, document, undefined);
