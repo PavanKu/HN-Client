fff79fa87e4622afdd5964cba093a810d447f34d
Pavan Kumar <pavankumar7860@gmail.com>
2015-07-20 09:17:43 +0530

    Using request and cheerio

--- public/app/app.js
+++ public/app/app.js
@@ -1,36 +1,55 @@
-(function (w, d, undefined) {
+/*global console, $*/
+(function (window, document, undefined) {
 	$(document).ready(function () {
+		var topStoryOffset = 0;
 
-		//Fetch HN Posts
-		$.getJSON("/getTopStories")
-			.done(function (stories) {
-				console.log(stories);
-				for (var i = 0; i < stories.length; i++) {
-					var storyId = stories[i];
-					$.getJSON("/getItem?id=" + storyId)
-						.done(function (story) {
-							console.log(story);
-							$("#topStoriesList").append(getCardMarkup(story));
+		$.getJSON("/top")
+			.done(function (data) {
+				var idsArr = JSON.parse(data);
+				for (; topStoryOffset < 20; topStoryOffset++) {
+					$.getJSON("/item?id=" + idsArr[topStoryOffset])
+						.done(function (data) {
+							console.log(data);
+							util.addStoryCard(util.getMarkup(data));
+						})
+						.fail(function (err) {
+							console.error(err);
 						});
 				}
+			})
+			.fail(function (err) {
+				console.error(err);
+			});
+	});
+
+	var util = {
+		addStoryCard: function (storyMarkup) {
+			$("#topStoriesList").append(storyMarkup);
+		},
+		getMarkup: function(story){
+			var storyJSON = story,
+				cardMarkup = '<div class="ui card" "data-id={id}"><div class="image"><img src="{image}"></div><div class="content"><a class="header" href={url}>{title}</a><div class="meta"><span class="date">{time}</span></div><div class="description">{description}</div></div><div class="extra content"><img class="ui avatar image " src={domainImg}><a>{domain}</a><a class="right floated"><i class="heart icon"></i>{score}</a><a class="right floated"><i class="comment icon"></i>{comment}</a></div></div>';
+			storyJSON.time = new Date(storyJSON.time).toUTCString();
+			storyJSON.comment = storyJSON.kids?storyJSON.kids.length : 0;
+			storyJSON.domainImg = util.getFaviconUrl(storyJSON.url);
+			storyJSON.domain = util.getDomain(storyJSON.url);
+
+			Object.keys(storyJSON).map(function(key){
+				var regx = new RegExp("{"+key+"}");
+				cardMarkup = cardMarkup.replace(regx, storyJSON[key]);
+				return;
 			});
 
-		function getCardMarkup(story) {
-			story.kids = story.kids || [];
-			var cardMarkup = '<div class="ui card"><div class="image"><img src="http://www.semantic-ui.com/images/avatar2/large/kristy.png">\
-							  </div><div class="content"><a class="header" href="'+story.url+'">'+story.title+'</a>\
-								<div class="meta"><span class="date">Joined in 2013</span></div><div class="description">Kristy is an art director living in New York.</div></div><div class="extra content"><img class="ui image mini avatar" src="'+getFavicon(story.url)+'"><a class="right floated"><i class="comment icon"></i>'+story.kids.length+' comments</a></div></div>';
-
 			return cardMarkup;
-		}
 
-		function getFavicon(url) {
-//						var service = "https://logo.clearbit.com/";
-//						var service = "http://favicon.yandex.net/favicon/";
-			var service = "//icons.duckduckgo.com/ip2/"
-			var domain = url.split("/")[2] || "";
-
-			return service + domain + ".ico";
+		},
+		getDomain: function(url){
+			return url.split("/")[2]
+		},
+		getFaviconUrl: function(url){
+			var service = "https://icons.duckduckgo.com/ip2/";
+			return service + util.getDomain(url) + ".ico";
 		}
-	});
+	}
+
 })(window, document, undefined);
