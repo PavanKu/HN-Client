a3d71bbd4360aaf9cf67e1ceac76a3aa53557c4c
Pavan Kumar <pavankumar7860@gmail.com>
2015-07-21 18:24:08 +0530

    Using request and cheerio

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-/*global require */
+/*global require, process */
 
 var express = require("express"),
 	path = require("path"),
 	fs = require("fs"),
-	request = require("request"),
-	cheerio = require("cheerio");
+	cheerio = require("cheerio"),
+	request = require("request");
 
-var app = express();
-
-var HN_baseurl = "https://hacker-news.firebaseio.com/v0/",
-	topStoryUrl = HN_baseurl + "topstories.json",
-	itemUrl = HN_baseurl + "item/";
+var app = express(),
+	hn_api = {
+		base: "https://hacker-news.firebaseio.com/v0/",
+		top: "topstories.json",
+		new: "newstories.json"
+	};
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -24,81 +25,64 @@ app.get("/", function (req, res) {
 	});
 });
 
-app.get("/getTopStories", function (req, res) {
+app.get("/item", function (req, res) {
+	var id = req.query.id,
+		story_url = hn_api.base + "item/" + id + ".json";
 
+	request({
+		uri: story_url
+	}, function (error, response, body) {
 
-	client.get(topStoryUrl, function (err, data) {
-		if (err) {
+		if (error) {
+			console.error(error);
 			return;
 		}
+		var storyJSON = JSON.parse(body);
+		console.log(storyJSON.url)
+//		res.json(storyJSON);
+		request({
+			uri: storyJSON.url
+		}, function(error, response, body){
+			try{
+				var $ = cheerio.load(body);
+				var fbDescription = $("meta[property=\"og:description\"]");
+				storyJSON.description = $(fbDescription).attr("content");
 
-		app.locals.topStoryIds = data;
-		app.locals.topStoryOffset = 0;
+				var fbImage = $("meta[property=\"og:image\"]");
+//				var firstImg = $("img").get(0);
+				storyJSON.image = $(fbImage).attr("content");// || $(firstImg).attr("src");
 
-		res.json(app.locals.topStoryIds.slice(app.locals.topStoryOffset, app.locals.topStoryOffset + 19));
-		app.locals.topStoryOffset += 20;
-		res.end();
+				res.json(storyJSON);
 
-		//
-		//		var getIteamUrl = "http://localhost:3000/getItems?offset=0&count=15";
-		//		client.get(getIteamUrl,function(err, data){
-		//			if(err) return;
-		//
-		//			res.json(data);
-		//			res.end();
-		//		});
-	});
-});
-
-app.get("/getNextTopStories", function (req, res) {
-
-	res.json(app.locals.topStoryIds.slice(app.locals.topStoryOffset, app.locals.topStoryOffset + 19));
-	app.locals.topStoryOffset += 20;
-	res.end();
-});
-
-app.get("/getItem", function (req, res) {
-	var itemId = req.query.id;
-
-	client.get([itemUrl, itemId, ".json"].join(""), function (err, data) {
-		if (err) {
-			return;
-		}
-
-		res.json(data);
-		res.end();
-	});
-});
-
-app.get("/getItems", function (req, res) {
-	console.log("Fetching Items");
-	var offset = req.query.offset;
-	var count = req.query.count;
-	var ticker = 0;
-	var storyData = [];
-
-	console.log("offset: ", offset, "count: ", count);
-	for (var i = offset; i < offset + count; i++) {
-		var storyId = app.locals.topStoryIds[i];
-		if (!storyId) break;
-		var itemUrl = [HN_baseurl, "item/", storyId, ".json"].join("");
-
-		client.get(itemUrl, function (err, data) {
-			if (err) return;
-
-			storyData.push(data);
-			ticker += 1;
-
-			if (ticker >= count) {
-				var nextUrl = '/getItems?offset=' + i + '&count=10'
-				storyData.push({
-					next: nextUrl
-				});
-				res.json(storyData);
-				res.end();
+			} catch(err){
+				console.error(err);
 			}
 		});
-	}
+	});
+});
+
+app.get("/top", function (req, res) {
+	request({
+		uri: hn_api.base + hn_api.top
+	}, function (error, response, body) {
+		if (error) {
+			console.error(error);
+			return;
+		}
+		res.jsonp(body);
+	});
+});
+
+app.get("/new", function (req, res) {
+	request({
+		uri: hn_api.base + hn_api.new
+	}, function (error, response, body) {
+		if (error) {
+			console.error(error);
+			return;
+		}
+		res.jsonp(body);
+	});
 });
 
 app.listen(Number(process.argv[2]));
